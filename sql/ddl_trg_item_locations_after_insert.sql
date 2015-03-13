72602a4d391efe879fe0cc0dd70e3c1a7e2b7f87
Juan Carlos Martinez <jcmartinez@mindbits.com.mx>
2015-03-13 17:07:02 -0600

    - Added productions catalog. - Fixed trigger for updating items existence.

--- sql/ddl_trg_item_locations_after_insert.sql
+++ sql/ddl_trg_item_locations_after_insert.sql
@@ -1,5 +1,12 @@
+USE `argos`;
+
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS argos.item_location_AFTER_INSERT$$
+USE `argos`$$
 CREATE TRIGGER `argos`.`item_location_AFTER_INSERT` AFTER INSERT ON `item_location`
 FOR EACH ROW
 begin
-    update argos.item set existence = (select sum(quantity) from argos.item_location where item_id = row.item_id) where item_id = row.item_id;
-end;
+    update argos.item set existence = (select sum(quantity) from argos.item_location where item_id = new.item_id) where item_id = new.item_id;
+end;$$
+DELIMITER ;
